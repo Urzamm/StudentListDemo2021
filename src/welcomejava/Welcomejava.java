36af8f9a2ce65f7fb2de8ed4d33fe450e1316efd
Urzam <Urzam@192.168.0.55>
2021-05-26 21:50:14 -0400

    initial commit

---
+++ src/welcomejava/Welcomejava.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package welcomejava;
+
+import java.util.ArrayList; 
+
+/**
+ *
+ * @author Urzam
+ */
+public class Welcomejava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+       String[]Students ={"Brock", "Tracy", "Ash", "Kiawe", "Gary"};
+       System.out.println(Students[4]);
+        
+    }
+    }
+
+   
+    
+
